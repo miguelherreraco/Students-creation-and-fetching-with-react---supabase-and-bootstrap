e39cd4df5cbf2402e20fcfdee29c3788b3ec8ac5
jotta5 <jotamigue5@gmail.com>
2024-02-09 15:00:36 -0500

    fix: warning message is now yellow (warning)

--- src/components/warning/Warning.tsx
+++ src/components/warning/Warning.tsx
@@ -3,7 +3,7 @@ import Alert from "react-bootstrap/esm/Alert";
 function Warning({message}:{message:string}){
 
     return (
-        <Alert className="mt-5">
+        <Alert className="mt-5" variant="warning">
             {message}        
         </Alert>
     )
